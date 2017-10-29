a27ea7f0396b9083cd1fe6018aba33355620db41
nerdmanship <johan@nerdmanship.com>
2017-10-29 17:40:21 +0800

    First commit

---
+++ js/plugins.js
@@ -0,0 +1,33 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+// When document is ready...
+$(document).ready(function() {
+  // Call Quovolver on the '.quotes' object
+  $('.quotes').quovolver({
+    autoPlaySpeed : 9000,
+    autoPlay : true,
+    pauseOnHover : false
+  });
+});
